fe5cf31df33c94006f666a2214946d262e7a9be2
Rafael Silva <rafasilva11987@gmail.com>
2025-06-02 14:07:24 -0300

    condicoes

---
+++ condicoes.py
@@ -0,0 +1,7 @@
+vendedor1 = int(input("Insira quantas vendas o  vendedor 1 fez: "))#variavel com o nome vendedor 1 atribuido int para colocar apenas numeros inteiros e input para receber os dados.
+vendedor2 = int(input("Insira quantas vendas o  vendedor 2 fez: "))
+
+if vendedor1 >= vendedor2: #condição: Se o vendedor 1 vender mais que o vendedor 2, condição verdadeira(True)
+    print("o vendedor 1 fez mais vendas  que o vendedor 2")
+else: #condição: caso a primeira condição não for verdadeira sera falsa(False).
+    print(print("o vendedor 2 fez mais vendas  que o vendedor 1"))    
